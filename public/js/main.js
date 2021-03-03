5d5fc128ccb2121fe8fcc0cf9011ee776b1beb16
raybags <baguma.github@gmail.com>
2021-03-03 22:36:28 +0100

    optimizing css

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 import {
-    move_to_top_of_page,
-    go_to_top,
-    openMenu,
-    closeMenu,
-    closeMenuWithLinkClicks,
-    copyRightYear,
-    cookie_accepted,
-    lazyLoadImages,
-    applyActiveClassOnCLick,
+  move_to_top_of_page,
+  go_to_top,
+  openMenu,
+  closeMenu,
+  closeMenuWithLinkClicks,
+  copyRightYear,
+  cookie_accepted,
+  lazyLoadImages,
+  applyActiveClassOnCLick,
 } from "./middleware.js";
 import { animateName, click_btn } from "./animations.js";
 import { dragDrop } from "./cards.js";
@@ -19,7 +19,7 @@ const image_with_name_to_animate = document.querySelector(".about-img");
 const name_container = document.querySelector(".name-animation");
 const project_in_mind_image = document.querySelector(".proj_in_mind");
 const project_in_mind_container = document.querySelector(
-    "#proj_in_mind_mainpage"
+  "#proj_in_mind_mainpage"
 );
 
 const cookie_accept_bunner = document.querySelector(".alert_box_accepted");
@@ -33,7 +33,6 @@ const img_links = document.querySelectorAll("#author-avator");
 const draggables = document.querySelectorAll("#card");
 const constainers = document.querySelectorAll("#card-list");
 
-const spinnerIcon = document.querySelector("#spinner");
 const nav_container = document.querySelector(".nav");
 const menu_bugger_icon = document.getElementById("menu_open_icon");
 const close_menu_icon = document.getElementById("menu_close_icon");
@@ -41,18 +40,17 @@ const all_links = document.querySelectorAll(".nav_link");
 const all_images = document.querySelectorAll("img.lazy");
 
 export {
-    allLinks,
-    topButton,
-    image_with_name_to_animate,
-    name_container,
-    nav_container,
-    menu_bugger_icon,
-    close_menu_icon,
-    cookie_accept_bunner,
-    cookie_btn,
-    cookie_accept_bunner_icon,
-    logo_btn,
-    spinnerIcon,
+  allLinks,
+  topButton,
+  image_with_name_to_animate,
+  name_container,
+  nav_container,
+  menu_bugger_icon,
+  close_menu_icon,
+  cookie_accept_bunner,
+  cookie_btn,
+  cookie_accept_bunner_icon,
+  logo_btn,
 };
 
 // Apply active class on menu link click handler.
